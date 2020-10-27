cb5da7660a105ab907690d04054b075b89cd3bd4
Harsh Shandilya <me@msfjarvis.dev>
2020-10-27 14:20:18 +0530

    core: set up dependencies
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,27 @@
+/*
+ * Copyright © 2014-2020 The Android Password Store Authors. All Rights Reserved.
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
 plugins {
     id("com.android.library")
     kotlin("android")
     `aps-plugin`
+}
+
+android {
+    sourceSets {
+        named("main").configure {
+            java.setSrcDirs(setOf("src/main/kotlin", "src/test/kotlin", "src/androidTest/kotlin"))
+        }
+    }
+}
+
+dependencies {
+    compileOnly(Dependencies.AndroidX.annotation)
+    implementation(project(":autofill-parser"))
+    implementation(Dependencies.AndroidX.core_ktx)
+    implementation(Dependencies.ThirdParty.kotlin_result)
+    implementation(Dependencies.ThirdParty.timber)
+    implementation(Dependencies.ThirdParty.timberkt)
 }
